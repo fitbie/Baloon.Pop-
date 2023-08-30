3e853e0afaf7d0f9cee2466d239a069674423e5a
fitbie <fitbiestudio@gmail.com>
2023-08-30 19:43:34 +1100

    Transfer

---
+++ Assets/Scripts/Baloons/Default/BaloonDefault.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaloonDefault : BaloonBase
+{
+    public override int Health { get; set; }
+    public override float SpeedModificator { get; set; }
+
+
+
+    public override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    public override void Flew()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    public override void Kill()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    public override void Pop()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    
+}
