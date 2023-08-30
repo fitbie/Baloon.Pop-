eb973af995b49377da9fba5e4e3ffa5957fce793
fitbie <fitbiestudio@gmail.com>
2023-08-31 07:28:03 +1100

    doc: tranfer summary

--- Assets/Scripts/Cinemachine/CallCMShake.cs
+++ Assets/Scripts/Cinemachine/CallCMShake.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
+
+/// <summary>
+/// Usually called from baloons prefab -> onDie UnityEvent in inspector.
+/// </summary>
 public class CallCMShake : MonoBehaviour
 {
     [SerializeField] private float shakeTime = 0.1f;
 
-    /// <summary>
-    /// Usually called from baloons prefab -> onDie UnityEvent in inspector.
-    /// </summary>
+
     public void Shake()
     {
         GameManager.Instance.cinemachineController.StartShake(shakeTime);
