b72638afb8a29c5ff6ca4f32de27b1d8e5be4d34
fitbie <fitbiestudio@gmail.com>
2023-08-30 21:17:29 +1100

    refactor: change mover

--- Assets/Scripts/Baloons/BaloonController.cs
+++ Assets/Scripts/Baloons/BaloonController.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(BaloonBase))]
+[RequireComponent(typeof(BaloonBase), typeof(Rigidbody2D))]
 public class BaloonController : MonoBehaviour
 {
     private BaloonBase baloon;
+    private Rigidbody2D rb;
 
 
     private void Start()
     {
-        if (TryGetComponent<BaloonBase>(out BaloonBase _baloon))
+        if (TryGetComponent<BaloonBase>(out BaloonBase _baloon) && TryGetComponent<Rigidbody2D>(out Rigidbody2D _rb))
         {
             baloon = _baloon;
-        }    
+            rb = _rb;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(0, baloon.speedModificator.CurrentSpeed);
     }
 }
