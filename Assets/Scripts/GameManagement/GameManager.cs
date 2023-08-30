0cd511303f2b5a64fa4cb1b3860cf3e8ade72e4e
fitbie <fitbiestudio@gmail.com>
2023-08-31 03:28:43 +1100

    feat: add game manager

---
+++ Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton Initialization
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null) { throw new ArgumentNullException("GameManager doesn't exist"); }
+
+            return instance; 
+        }
+    }
+
+    private void Awake() 
+    {
+        instance = this;
+    }
+
+    #endregion
+
+
+    public BaloonsSpawner baloonsSpawner;
+}
