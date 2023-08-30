b9a918aa9e594b99cc936d03886db0e4b2347a0d
fitbie <fitbiestudio@gmail.com>
2023-08-31 05:34:14 +1100

    style: delete log

--- Assets/Scripts/Scene/DeathZone.cs
+++ Assets/Scripts/Scene/DeathZone.cs
@@ -5,7 +5,6 @@ public class DeathZone : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("Collide");
         if (other.gameObject.TryGetComponent<BaloonBase>(out BaloonBase baloon))
         {
             baloon.Die();
