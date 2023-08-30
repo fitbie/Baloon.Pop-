abe695154e86a8fc1546dd7fb1dbc9f692d35628
fitbie <fitbiestudio@gmail.com>
2023-08-31 03:27:14 +1100

    feat: add baloonbomb

---
+++ Assets/Scripts/Baloons/Bomb/BaloonBomb.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class BaloonBomb : BaloonBase
+{
+    public void Start()
+    {
+        health.SetHealth();
+        speedModificator.SetSpeed();
+    }
+    
+
+    public override void Kill()
+    {
+        base.Kill();
+        
+        GameScore.ModifyScore(scorePointsModificator.ScorePenalty);
+
+        Vibration.Vibrate(200);
+        GameObject.Destroy(gameObject);
+    }
+
+
+    public override void Flew()
+    {
+        base.Flew();
+
+        Die();
+    }
+
+
+    public override void Die()
+    {
+        base.Die();
+        
+        GameScore.ModifyScore(scorePointsModificator.ScoreReward);
+
+        GameObject.Destroy(gameObject);
+    }
+
+}
