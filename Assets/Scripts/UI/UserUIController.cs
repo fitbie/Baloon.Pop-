8b38e584d02307f05ef0c7f7f88672ff8732b72e
fitbie <fitbiestudio@gmail.com>
2023-09-01 08:39:00 +1100

    style: delete comment

--- Assets/Scripts/UI/UserUIController.cs
+++ Assets/Scripts/UI/UserUIController.cs
@@ -10,10 +10,11 @@ public class UserUIController : MonoBehaviour
     [SerializeField] private GameObject pausePanel;
 
 
-    // Called from inspector Pause/Continue buttons on UserUI.
+
     public void ShowPauseUI(bool state)
     {
         if (pauseButton.interactable == state) { pauseButton.interactable = !state; } // button interactable should be opposite to pause state 
         if (pausePanel.activeSelf != state) { pausePanel.SetActive(state); }
     }
+
 }
