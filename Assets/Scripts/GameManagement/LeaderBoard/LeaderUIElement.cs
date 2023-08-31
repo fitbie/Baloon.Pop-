f5b6a6a8a4db96349c1921d5c2062d99cb44952c
fitbie <fitbiestudio@gmail.com>
2023-09-01 08:15:50 +1100

    feat: add single leaderbord ui  log

---
+++ Assets/Scripts/GameManagement/LeaderBoard/LeaderUIElement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderUIElement : MonoBehaviour
+{
+    public Text playerName;
+    public Text playerScore;
+}
