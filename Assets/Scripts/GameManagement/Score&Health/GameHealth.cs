7ae1bdea6a91b9c8eb6b7cce3e2a53da514f74c9
fitbie <fitbiestudio@gmail.com>
2023-09-01 17:08:26 +1100

    Transfer

--- Assets/Scripts/GameManagement/Score&Health/GameHealth.cs
+++ Assets/Scripts/GameManagement/Score&Health/GameHealth.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class GameHealth : MonoBehaviour
+public class GameHealth
 {
     public static int MaxHealth { get; private set; } = 3;
     public static int Health { get; private set; }
 
 
-    public static void RemoveHP(int value)
+    public static void Modify(int value)
     {
         Health += value;
 
